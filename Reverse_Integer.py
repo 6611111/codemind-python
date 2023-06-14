05bd334726a64c9f6e93e3e8d56490e016ba0087
Anusha Yelleti <119839788+6611111@users.noreply.github.com>
2023-06-14 09:35:14 -0700

    def reverseNum(n):  #user-defined function    if n >= 0:       return int(str(n)[::-1])    else:       return int('-{val}'.format(val = str(n)[1:][::-1]))
    # take inputs
    num = int(input())
    
    # calling function and display result
    print(reverseNum(num))

---
+++ Reverse_Integer.py
@@ -0,0 +1,11 @@
+def reverseNum(n):  #user-defined function
+   if n >= 0: 
+      return int(str(n)[::-1])
+   else:
+      return int('-{val}'.format(val = str(n)[1:][::-1]))
+
+# take inputs
+num = int(input())
+
+# calling function and display result
+print(reverseNum(num))
