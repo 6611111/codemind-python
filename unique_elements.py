a3f7504dbb246f775645132520195b09d29fbf76
Anusha Yelleti <119839788+6611111@users.noreply.github.com>
2023-07-02 16:32:01 +0530

    n=int(input()) lst=list(map(int,input().split())) l=[] for i in lst:     if i not in l:         l.append(i) print(*l)

--- unique_elements.py
+++ unique_elements.py
@@ -1,8 +1,7 @@
 n=int(input())
-lst=list(input().split())
-res_l=[]
+lst=list(map(int,input().split()))
+l=[]
 for i in lst:
-    if i not in res_l:
-        res_l.append(i)
-for i in res_l:
-    print(i,end=" ")
+    if i not in l:
+        l.append(i)
+print(*l)
