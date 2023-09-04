b2e19702b898612fc3dd76100bfefde5aef7d768
Anusha Yelleti <119839788+6611111@users.noreply.github.com>
2023-09-04 03:11:59 -0700

    s=input() for i in s:     if(s.count(i)==1):         print(s.index(i))         break else:     print("-1")

--- First_Unique_Character_in_a_String.py
+++ First_Unique_Character_in_a_String.py
@@ -1,11 +1,7 @@
-def unique(s):
-    from collections import Counter
-    c=Counter(s)
-    for i,j in enumerate(s):
-        if c[j]==1:
-            return i
-    else:
-        return -1
 s=input()
-r=unique(s)
-print(r)
+for i in s:
+    if(s.count(i)==1):
+        print(s.index(i))
+        break
+else:
+    print("-1")
